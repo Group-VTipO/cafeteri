f71d075281205d4e94aef77cad6f39c9bda10147
Meyrambek <m.zhusipp@gmail.com>
2023-02-15 13:19:17 +0600

    class Menu
    класс меню и продукты

---
+++ Brewtopia-Org/Source/pythony/menu.py
@@ -0,0 +1,35 @@
+class Menu:
+    def __init__(self):
+        self.items = {}
+
+    def add_item(self, name, price, description):
+        self.items[name] = {
+            'price': price,
+            'description': description
+        }
+
+    def remove_item(self, name):
+        del self.items[name]
+
+    def get_items(self):
+        return self.items
+
+
+# обьект
+menu = Menu()
+
+# внесение меню в Объект
+menu.add_item('Cappuccino', 3.50, 'Espresso with frothed milk')
+menu.add_item('Latte', 4.00, 'Espresso with steamed milk')
+menu.add_item('Mocha', 4.50, 'Espresso with chocolate and steamed milk')
+
+# Получить текущие пункты меню
+items = menu.get_items()
+
+# Print menu vse
+for name, data in items.items():
+    print(name)
+    print('-' * len(name))
+    print('Price: ${:.2f}'.format(data['price']))
+    print(data['description'])
+    print()
